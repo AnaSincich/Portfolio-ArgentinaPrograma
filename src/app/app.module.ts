2ac999ddf97d6f38f0e1ddfad5842e36a32cb5ab
AnaSincich <ana.sincich@gmail.com>
2023-02-03 16:36:02 -0300

    Componentes header, banner y acerca de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
